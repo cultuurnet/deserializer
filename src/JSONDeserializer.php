8935ab80635482107a99b5a441943ecf8af62401
Kristof Coomans <kristof@2dotstwice.be>
2016-03-24 12:03:28 +0100

    III-844: Add assoc option to JSONDeserializer

--- src/JSONDeserializer.php
+++ src/JSONDeserializer.php
@@ -5,19 +5,34 @@
 
 namespace CultuurNet\Deserializer;
 
-use ValueObjects\String\String;
+use ValueObjects\String\String as StringLiteral;
 
 class JSONDeserializer implements DeserializerInterface
 {
     /**
+     * When true, returned objects will be converted into associative arrays.
+     *
+     * @var bool
+     */
+    private $assoc;
+
+    /**
+     * @param bool $assoc
+     */
+    public function __construct($assoc = false)
+    {
+        $this->assoc = $assoc;
+    }
+
+    /**
      * Decodes a JSON string into a generic PHP object.
      *
-     * @param String $data
+     * @param StringLiteral $data
      * @return \stdClass
      */
-    public function deserialize(String $data)
+    public function deserialize(StringLiteral $data)
     {
-        $data = json_decode($data->toNative());
+        $data = json_decode($data->toNative(), $this->assoc);
 
         if (null === $data) {
             throw new NotWellFormedException('Invalid JSON');
